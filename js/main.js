3b877813590bd3f6935bd7c456c61cc9fb3b32e1
leoroca98 <leoroca12345@gmail.com>
2023-07-04 16:33:52 +0200

    devo correggere un errore

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 // come prima cosa stampo i numeri da 1 a 100
+// successivamente 
+//
+//
 
 for (let i = 1; i <= 100; i++); {
-console.log(i);
+    
+
+if (i % 3 == 0 && i % 5 == 0) {
+    console.log("fizzbuzz");
+
+} else if (i % 3 == 0) {
+    console.log("fizz");
+
+} else if (i % 5 == 0) {
+    console.log ("buzz");
+
+    } else {
+        console.log(i); 
+    }
+     
+
+
 }
 
 
